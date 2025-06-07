c053dbbcb5f75e0afcc0be28ebdc6bb4d18d8032
DanielWalnut <45447813+hetaoBackend@users.noreply.github.com>
2025-06-07 20:48:39 +0800

    feat: support to adjust writing style (#290)
    * feat: implment backend for adjust report style
    
    * feat: add web part
    
    * fix test cases
    
    * fix: fix typing
    
    ---------
    
    Co-authored-by: Henry Li <henry1943@163.com>

--- web/src/app/chat/components/input-box.tsx
+++ web/src/app/chat/components/input-box.tsx
@@ -104,7 +104,7 @@ export function InputBox({
         />
       </div>
       <div className="flex items-center px-4 py-2">
-        <div className="flex grow">
+        <div className="flex grow gap-2">
           <Tooltip
             className="max-w-60"
             title={
@@ -133,6 +133,7 @@ export function InputBox({
               <Detective /> Investigation
             </Button>
           </Tooltip>
+          <ReportStyleDialog />
         </div>
         <div className="flex shrink-0 items-center gap-2">
           <Tooltip title={responding ? "Stop" : "Send"}>
